fe8644aa8fbbe97faffb0e9ec6e58cec4d6e937e
Ryan Haslag <rchaslag@liveatbrookside.com>
2016-11-17 11:14:52 -0600

    Controller redirection to specific form contact type views created. Skeleton of each contact-specific view created.

--- app/Http/Controllers/CreateNewCustomer/CreateNewCustomerController.php
+++ app/Http/Controllers/CreateNewCustomer/CreateNewCustomerController.php
@@ -11,11 +11,68 @@ class CreateNewCustomerController extends Controller
         return view('createNewCustomer.start');
     }
 
-    public function contactInformation() {
-        return view('createNewCustomer.contactInformation');
+    public function directForm(Request $request) {
+        $customerContactType = $request->input('customerContact');
+
+        switch($customerContactType) {
+            //Inquiry Customers
+            case "email":
+            case "phone":
+            case "specialEvent":
+                $inquiryCustomer = $request->input('inquiryCustomer');
+                //Determine if the customer has an inquiry
+                if($inquiryCustomer == 'yes') {
+                    //Redirect to specific form contact input
+                    $redirectionUrl = $this->loadContactInformation($customerContactType);
+                    return redirect($redirectionUrl);
+                }
+                break;
+            //Leasing Customers
+            case "walkIn":
+                $leasingCustomerAppointment = $request->input('leasingCustomerAppointment');
+                //Determine if the customer has an appointment
+                echo $leasingCustomerAppointment;
+                break;
+            default:
+                return back();
+                break;
+        }
+        return back();
     }
 
-    public function customerPreferences() {
-        echo "Customer preferences reached.";
+    /*
+     * Function: Determine the user's appropriate contact
+     * information form based on contact type
+     *
+     * @param $contactType | string redirect user to appropriate form
+     */
+    private function loadContactInformation($contactType) {
+        //Determine contact type-specific form views
+        switch($contactType) {
+            case "email":
+                return '/createNewCustomer/emailInquiry';
+            break;
+            case "phone":
+                return '/createNewCustomer/phoneInquiry';
+            break;
+            case "specialEvent":
+                return '/createNewCustomer/specialEventInquiry';
+            break;
+            default:
+                return '/';
+            break;
+        }
+    }
+
+    public function emailInquiry() {
+        return view('createNewCustomer.emailInquiry');
+    }
+
+    public function phoneInquiry() {
+        return view('createNewCustomer.phoneInquiry');
+    }
+
+    public function specialEventInquiry() {
+        return view('createNewCustomer.specialEventInquiry');
     }
 }
